e64fce809ee2f8331175514672e5b4c494369211
wangbin <373202015@qq.com>
2013-05-21 19:30:50 +0800

    a simple android keybox convertor tool. it will read 300000 lines from keybox.txt file,and convert each line to the keybox.Finally it convert the keybox to csv format and write them to the keybox_csv_N.csv files in the current directory, each output file will have 50000 keyboxes.

---
+++ main.cpp
@@ -0,0 +1,73 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "keybox.h"
+
+#define KEYBOX_FILE_NAME "KeyBox.txt"
+#define DEST_FILE_NAME_FORMAT "keybox_csv_%d.csv"
+#define BUFFER_LENGTH 512
+#define NUMBER_OF_KEYBOXES 300000
+#define NUMBER_OF_EACH_FILE 50000
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    char buffer[BUFFER_LENGTH];
+    char destFileName[20];
+    FILE* src = NULL;
+    FILE* dest = NULL;
+    Keybox keybox;
+    cout<<"KeyboxConvertor opened"<<endl<<endl;
+    
+    if((src = fopen(KEYBOX_FILE_NAME, "r")) == NULL)
+    {
+           cout<<"KeyboxConvertor open source file FAILED"<<endl;
+           return 1;
+    }
+    for(int i=0; i<NUMBER_OF_KEYBOXES; i++)
+    {
+        //open a new destination file to write
+        if( i%NUMBER_OF_EACH_FILE == 0 )
+        {
+            if(dest != NULL)
+            {
+                fclose(dest);
+                dest = NULL;
+            }
+            sprintf(destFileName, DEST_FILE_NAME_FORMAT, i/NUMBER_OF_EACH_FILE);
+            cout<<"KeyboxConvertor : create dest file "<<destFileName<<endl;
+            if((dest = fopen(destFileName, "wb")) == NULL)
+            {
+                cout<<"KeyboxConvertor open destination file FAILED"<<endl;
+                return 1;
+            }
+        }
+        
+        //read a line from source file 
+        memset(buffer, 0,  BUFFER_LENGTH);
+        if(fgets(buffer, BUFFER_LENGTH, src) == NULL)
+        {
+            cout<<"KeyboxConvertor read file FAILED"<<endl;
+            return 1;
+        }
+        //cout<<"KeyboxConvertor handle keybox : "<<buffer<<endl;
+        keybox.init(buffer);
+        memset(buffer, 0,  BUFFER_LENGTH);
+        int length = keybox.convertToCsv(buffer);
+        char deviceID[DEVICEID_SIZE_IN_KEYBOX];
+        keybox.getDeviceID(deviceID);
+        printf("KeyboxConvertor : converting keybox %s\r\n", deviceID);
+        
+        //write the formated keybox to destination file
+        fwrite(buffer, 1, length, dest);
+    }
+    
+finally:
+    //close all opened files
+    fclose(src);
+    fclose(dest);
+    
+    //system("PAUSE");
+    return EXIT_SUCCESS;
+}
